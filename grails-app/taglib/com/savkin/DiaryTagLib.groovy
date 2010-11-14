a6ec6e055b504217631151981676e17e777e1211
Victor Savkin <avix1000@gmail.com>
2010-11-14 13:13:41 -0500

    improved: parametrised my fancy textarea

--- grails-app/taglib/com/savkin/DiaryTagLib.groovy
+++ grails-app/taglib/com/savkin/DiaryTagLib.groovy
@@ -6,14 +6,18 @@ class DiaryTagLib {
 	def autoresizedTextArea = {attrs, body->
 		def pathToJsLib = createLinkTo(dir:'js/jquery',file:'autoresize.jquery.min.js')
 		out << "<script type=\"text/javascript\" src=\"${pathToJsLib}\"></script>"
-		out << jq.jquery([:], {'''
-			$('#text').autoResize({
-				onResize : function() {$(this).css({opacity:0.8});},
-				animateCallback : function() {$(this).css({opacity:1});},
+		
+		def elementId = "#${attrs.name}"
+		def js = """
+			\$('$elementId').autoResize({
+				onResize : function() {\$(this).css({opacity:0.8});},
+				animateCallback : function() {\$(this).css({opacity:1});},
 				animateDuration : 100,
 				extraSpace : 40,
 				animate: true
-			});'''})
+			});"""
+		
+		out << jq.jquery([:], {js})
 		out << g.textArea(name: attrs.name, value: attrs.value)
 	}
 }
